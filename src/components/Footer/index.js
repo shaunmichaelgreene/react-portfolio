292c4ebe811c4f52115cd804045044b810084f9f
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-08-14 16:30:49 -0400

    updating components folder structure for first test build

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div>
+            <p>Thank you for visiting!</p>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from '../Navigation';
+
+function Header() {
+    return (
+        <div>
+            <p>Shaun's Portfolio</p>
+            <Navigation></Navigation>
+        </div>
+    )
+}
+
+export default Header;
