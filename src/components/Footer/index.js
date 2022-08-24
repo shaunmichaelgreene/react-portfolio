0b1a5ce9568520caba00b4a1dbf267f11aa97b48
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-08-24 18:09:32 -0400

    updating footer to correct error

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,9 +3,18 @@ import React from 'react';
 function Footer() {
     return (
         <div>
+            <p>
+                <span>&copy;</span> 2022 Shaun Greene  |  
+                <img src={require`../../assets/icons/email-icon.jpeg`} className="inline" alt="Email icon"></img>
+                </p>
             <p>Thank you for visiting!</p>
         </div>
     )
 }
 
 export default Footer;
+
+{/* <img src={require`../../assets/icons/email-icon.jpeg`} className="inline" alt="Email icon"></img>
+                <a href="mailto:shaunmichaelgreene@gmail.com">shaunmichaelgreene@gmail.com</a>  |  
+                <img src={require`../../assets/images/linkedin-icon.png`} className="inline"  alt="LinkedIn icon"></img>
+                <a href="https://www.linkedin.com/in/shaunmichaelgreene/">shaunmichaelgreene</a>  |  <a href="https://github.com/shaunmichaelgreene">shaunmichaelgreene</a></p> */}
