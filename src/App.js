be26e3aaeeebe9bfda3ff7daf5ebdbd69032148b
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-08-31 17:17:38 -0400

    updated nav bar and home page to fix conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,59 +1,84 @@
-import React, { useState } from 'react';
-import Header from './components/Header';
+import React, { useState } from "react";
+import Header from "./components/Header";
 // import Navigation from './components/Navigation';
-import About from './components/About';
-import Portfolio from './components/Portfolio';
-import Resume from './components/Resume';
-import Footer from './components/Footer';
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Resume from "./components/Resume";
+import Footer from "./components/Footer";
 // import Project from './components/Project';
-import ContactForm from './components/Contact';
+import ContactForm from "./components/Contact";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-    const [categories] = useState([ 
+    const [categories] = useState([
         {
-          name: 'Front-End',
-          description: 'Projects built with HTML, CSS, JavaScript, and APIs with a focus on front-end development',
+            name: "Front-End",
+            description:
+                "Projects built with HTML, CSS, JavaScript, and APIs with a focus on front-end development",
         },
-        { name: 'OOP-ORM-TDD', description: 'Projects built with a focus on Object-Oriented Programming, Object Relational Mapping, and Test-Driven Development' },
-        { name: 'Back-End', description: 'Projects with a focus on back-end development, featuring SQL, NoSQL, MVC' },
-        { name: 'Full-Stack', description: 'Full Stack projects built with a MERN state of mind' },
-      ]);
-    
-      const [currentCategory, setCurrentCategory] = useState(categories[0]);
-    
+        {
+            name: "OOP-ORM-TDD",
+            description:
+                "Projects built with a focus on Object-Oriented Programming, Object Relational Mapping, and Test-Driven Development",
+        },
+        {
+            name: "Back-End",
+            description:
+                "Projects with a focus on back-end development, featuring SQL, NoSQL, MVC",
+        },
+        {
+            name: "Full-Stack",
+            description: "Full Stack projects built with a MERN state of mind",
+        },
+    ]);
+
+    const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
     //   const [contactSelected, setContactSelected] = useState(false);
 
     return (
-        <div>
-            <Header
-            // categories={categories}
-            // setCurrentCategory={setCurrentCategory}
-            // currentCategory={currentCategory}
-            // contactSelected={contactSelected}
-            // setContactSelected={setContactSelected}
-            >
-                    
-                {/* <Navigation
-                    categories={categories}
-                    setCurrentCategory={setCurrentCategory}
-                    currentCategory={currentCategory}
-                    contactSelected={contactSelected}
-                    setContactSelected={setContactSelected}
-                ></Navigation> */}
-            </Header>
+        // <div>
+        //     <Header
+        //     // categories={categories}
+        //     // setCurrentCategory={setCurrentCategory}
+        //     // currentCategory={currentCategory}
+        //     // contactSelected={contactSelected}
+        //     // setContactSelected={setContactSelected}
+        //     >
 
-            <main>
-                {/* {!contactSelected ? ( */}
-                    <>
-                        <Portfolio currentCategory={currentCategory}></Portfolio>
-                        <Resume></Resume>
-                        <About></About>
-                        <ContactForm></ContactForm>
-                    </>
-                {/* ) : (
-                )} */}
-            </main>
-            <Footer></Footer>
+        //         {/* <Navigation
+        //             categories={categories}
+        //             setCurrentCategory={setCurrentCategory}
+        //             currentCategory={currentCategory}
+        //             contactSelected={contactSelected}
+        //             setContactSelected={setContactSelected}
+        //         ></Navigation> */}
+        //     </Header>
+
+        //     <main>
+        //         {/* {!contactSelected ? ( */}
+        //             <>
+        //                 <Portfolio currentCategory={currentCategory}></Portfolio>
+        //                 <Resume></Resume>
+        //                 <About></About>
+        //                 <ContactForm></ContactForm>
+        //             </>
+        //         {/* ) : (
+        //         )} */}
+        //     </main>
+        //     <Footer></Footer>
+        // </div>
+        <div>
+            <Router>
+                <Header />
+                <Routes>
+                    <Route exact path="/" element={<About />} />
+                    <Route exact path="/portfolio" element={<Portfolio currentCategory={currentCategory} />} />
+                    <Route exact path="/resume" element={<Resume />} />
+                    <Route exact path="/contact" element={<ContactForm />} />
+                </Routes>
+                <Footer/>
+            </Router>
         </div>
     );
 }
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -17,26 +17,26 @@ function Navigation(props) {
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a data-testid="about" href="#about">
+            <a data-testid="about" href="/">
             {/* <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}> */}
               About me
             </a>
           </li>
           <li className="mx-2">
-            <a data-testid="portfolio" href="#portfolio">
+            <a data-testid="portfolio" href="/portfolio">
             {/* <a data-testid="portfolio" href="#portfolio" onClick={() => setContactSelected(false)}> */}
               Portfolio
             </a>
           </li>
           <li className="mx-2">
-            <a data-testid="resume" href="#resume">
+            <a data-testid="resume" href="/resume">
             {/* <a data-testid="resume" href="#resume" onClick={() => setContactSelected(false)}> */}
               Resume
             </a>
           </li>
           {/* <li className={`mx-2 ${contactSelected && 'navActive'}`}> */}
           <li className="mx-2">
-            <a data-testid="contact" href="#contact">
+            <a data-testid="contact" href="/contact">
             {/* <a data-testid="contact" href="#contact" onClick={() => setContactSelected(true)}> */}
               Contact
               </a>
